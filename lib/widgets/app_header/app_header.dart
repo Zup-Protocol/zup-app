51504f1684bddeb2a34e50fb0b339298b4dd5817
Ryan Holanda <ryanholanda08@gmail.com>
2025-03-20 13:14:59 -0300

    test cache fix

--- lib/widgets/app_header/app_header.dart
+++ lib/widgets/app_header/app_header.dart
@@ -53,7 +53,7 @@ class _AppHeaderState extends State<AppHeader> with DeviceInfoMixin {
           ClipRect(
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-              child: Container(color: Colors.blueAccent
+              child: Container(color: Colors.black
                   // .withValues(alpha: 0.85)
                   ),
             ),
