40ea5419f3f4715747af25b96319f646c39bbb32
Ryan Holanda <ryanholanda08@gmail.com>
2026-05-12 09:12:30 -0300

    Update banner: stopped operations

--- lib/app/app_layout.dart
+++ lib/app/app_layout.dart
@@ -54,7 +54,7 @@ class _AppPageState extends State<AppPage> with DeviceInfoMixin {
           padding: const EdgeInsets.only(left: 20, right: 10, bottom: 5, top: 5),
           dividerColor: Colors.transparent,
           content: const Text(
-            "⚠️ We're having issues with pools on Base Network (Yields are delayed). Team is working on it.",
+            "We have stopped operations. Thank you for your support.",
             style: TextStyle(color: ZupColors.orange),
           ),
           actions: [
